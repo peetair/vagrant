c28ab540ea8befac343eb9f19f79ca11f9bbcdad
pborbas <peter.borbas@yahoo.com>
2015-01-03 11:41:33 +0100

    add grant privileges to root

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ echo "mysql-server mysql-server/root_password_again password $DBPASSWD" | debcon
 sudo apt-get update
 sudo apt-get -y install mysql-server-5.5
 
-mysql -uroot -p$DBPASSWD -e "grant all privileges on *.* to 'root'@'localhost' identified by '$DBPASSWD'"
-mysql -uroot -p$DBPASSWD -e "grant all privileges on *.* to 'root'@'%' identified by '$DBPASSWD'"
+mysql -uroot -p$DBPASSWD -e "grant all privileges on *.* to 'root'@'localhost' identified by '$DBPASSWD' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+mysql -uroot -p$DBPASSWD -e "grant all privileges on *.* to 'root'@'%' identified by '$DBPASSWD' WITH GRANT OPTION; FLUSH PRIVILEGES;"
 sudo sed -i '/127.0.0.1/c\bind-address = 0.0.0.0' /etc/mysql/my.cnf
 sudo service mysql restart
